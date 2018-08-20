1ea93f3be28fc7ef5c35aeeaa7bd311d7471b697
Alexander <labianca.alex@gmail.com>
2018-08-19 19:14:58 -0500

    driver updates and added an rpcAdapter to test things out

--- driver.js
+++ driver.js
@@ -18,12 +18,36 @@ const c1 = new Contact(other, 'Hello');
 const c2 = new Contact(other2, "World");
 const c3 = new Contact(other3, "Hi");
 const c4 = new Contact(other4, "There");
+const c5 = new Contact(NodeId.generateRandomId(), "akdfj");
+const c6 = new Contact(NodeId.generateRandomId(), "akjfdklajfkl");
+const c7 = new Contact(NodeId.generateRandomId(), "dkfjksakk");
+const c8 = new Contact(NodeId.generateRandomId(), "akldjfjalfjksajldkfjklafj");
 
+const contacts = [c1,c2,c3,c4,c5,c6,c7,c8];
 
 const dht1 = DHT.bootstrap();
-const dht2  = DHT.bootstrap(dht1._id);
+//const dht2  = DHT.bootstrap(dht1._id);
 
+function addContacts(contacts,done) {
 
+    function iterate(index) {
+        if(index === contacts.length) {
+            console.log('Initialized DHT');
+            console.log(dht1._routingTable.toString())
+            return done();
+        }
+
+        dht1.store(contacts[index], (added)=>{
+            iterate(index+1);
+        });
+    }
+
+    iterate(0);
+}
+
+addContacts(contacts, ()=>{
+    const dht2 = DHT.bootstrap(dht1._id);
+})
 
 //const dht = new DHT();
 // for(let i = 0; i < 160; i++) {
---
+++ rpcAdapter.js
@@ -0,0 +1,13 @@
+
+
+function RpcAdapter() {
+
+}
+
+RpcAdapter.prototype.RPC_findNode = function(nodeId) {
+
+}
+
+RpcAdapter.prototype.RPC_store = function() {}
+RpcAdapter.prototype.RPC_ping = function() {}
+RpcAdapter.prototype.RPC_findValue = function() {}
